7557d18a6f2f523168eb2235d6148c3a8b1a01b2
Mayreeel <kuwon15@gmail.com>
2023-02-21 23:14:27 +0900

    코테 13일차 문제풀이

---
+++ DAY13.js
@@ -0,0 +1,8 @@
+function solution(emergency) {
+  var answer = [];
+  let arr = [...emergency].sort((a, b) => b - a);
+  for (let i = 0; i < emergency.length; i++) {
+    answer.push(arr.indexOf(emergency[i]) + 1);
+  }
+  return answer;
+}
